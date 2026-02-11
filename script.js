a5ab78d17374ea8f95433c32f8120d82de17c12a
charlesnikomanalo-wq <charlesnikomanalo@gmail.com>
2026-02-11 21:49:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const envelope = document.getElementById("envelope");
+const letterPreview = document.getElementById("letterPreview");
+const fullLetter = document.getElementById("fullLetter");
+
+let state = 0; // 0 = closed, 1 = preview, 2 = full letter
+
+envelope.addEventListener("click", () => {
+  if (state === 0) {
+    envelope.classList.add("open");
+    envelope.classList.add("peek");
+    state = 1;
+  } else if (state === 1) {
+    letterPreview.style.opacity = 0;
+    fullLetter.classList.add("show");
+    state = 2;
+  }
+});
